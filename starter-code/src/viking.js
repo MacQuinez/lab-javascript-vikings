da29f7f7b42a58e90d501e0e3a76994fc4ae3bc7
Javier Marquinez <emarquinez@gmail.com>
2017-11-07 21:34:33 +0100

    initial commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,24 @@
 // Soldier
-function Soldier () {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+Soldier.prototype.receiveDamage = function(damage) {
+  return (this.health = this.health - damage);
+};
 
 // Viking
-function Viking () {}
+function Viking(name, strength, health) {
+  this.name = name;
+  this.strength = strength;
+  this.health = health;
+}
 
 // Saxon
-function Saxon () {}
+function Saxon() {}
 
 // War
-function War () {}
+function War() {}
